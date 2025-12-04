5b440d2683f3955a4f415068723cda12d60b0f1f
Titi <tristanfio05@gmail.com>
2025-12-04 14:58:24 +0100

    [DEV]: Dictionary finished & restructuration project folder

--- main.py
+++ main.py
@@ -1,55 +1,11 @@
 ''' Python version : 3.13.9 '''
 
-import os
-import json
-from bs4 import BeautifulSoup
+from src.retrieve_data_file import RetrieverData
 
 def main() -> None:
 
-    data_xml = os.path.join(
-        os.getcwd(),
-        "data",
-        "xml",
-        "essence",
-        "PrixCarburants_instantane.xml"
-    )
-    
-    with open(data_xml, "r") as file:
-        data = file.read()
-
-    soup = BeautifulSoup(data, "xml")
-    
-    pdv_beacon = soup.find_all("pdv")
-    for pdv in pdv_beacon:
-        if pdv.find('adresse'):
-            address_beacon = pdv.find('adresse')
-        if pdv.find('ville'):
-            city_beacon = pdv.find('ville')
-        if pdv.find_all('prix'):
-            price_beacon = pdv.find_all('prix')
-
-    fuel_name_beacon, fuel_price_beacon = [], []
-    for price in price_beacon:
-        if price.get('nom'):
-            fuel_name_beacon.append(price.get('nom'))
-            fuel_price_beacon.append(price.get('valeur'))
-
-    dictionnary_fuel = {}
-    for city, address, fuel_name, fuel_price in zip(city_beacon, address_beacon, fuel_name_beacon, fuel_price_beacon):
-        if city.text not in dictionnary_fuel:
-            dictionnary_fuel[city.text] = {
-                address.text: {}
-            }
-
-        # else:
-        #     if address.text not in dictionnary_fuel[city.text]:
-        #         dictionnary_fuel[city.text][address.text] = {}
-
-    os.makedirs('output', exist_ok=True)
-    with open('output/fuel_data.json', 'w', encoding='utf-8') as json_file:
-        json.dump(dictionnary_fuel, json_file, ensure_ascii=False, indent=4)
-
-    return None
+    retriever = RetrieverData()
+    retriever.retrieve_data_fuel_method()
 
 if __name__ == "__main__":
     main()
---
+++ src/retrieve_data_file.py
@@ -0,0 +1,49 @@
+import os
+from bs4 import BeautifulSoup
+
+class RetrieverData:
+    ''' Class to retrieve data from different extension files '''
+
+    def retrieve_data_fuel_method(self) -> dict:
+        ''' Method to retrieve fuel data from an XML file '''
+
+        data_xml = os.path.join(
+            os.getcwd(),
+            "data",
+            "xml",
+            "essence",
+            "PrixCarburants_instantane.xml"
+        )
+
+        with open(data_xml, "r") as file:
+            data = file.read()
+
+        soup = BeautifulSoup(data, "xml")
+
+        pdv_beacon = soup.find_all("pdv")
+
+        dictionary_fuel = {}
+        for pdv in pdv_beacon:
+            city_tag = pdv.find('ville')
+            address_tag = pdv.find('adresse')
+
+            if not city_tag or not address_tag:
+                continue
+
+            city = city_tag.text.strip()
+            address = address_tag.text.strip()
+
+            if city not in dictionary_fuel:
+                dictionary_fuel[city] = {}
+
+            if address not in dictionary_fuel[city]:
+                dictionary_fuel[city][address] = {}
+
+            for price in pdv.find_all('prix'):
+                fuel_name = price.get('nom')
+                fuel_price = price.get('valeur')
+
+                if fuel_name and fuel_price:
+                    dictionary_fuel[city][address][fuel_name] = fuel_price
+
+        return dictionary_fuel
